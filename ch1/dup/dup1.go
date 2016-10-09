acf7403b49d6fef19c1e5140393049664230c4e4
ArchKudo <vgd.vyas17@gmail.com>
2016-10-09 13:51:05 +0530

    Added dup

---
+++ ch1/dup/dup1.go
@@ -0,0 +1,30 @@
+// dup1.go Prints duplicate string and their count.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// map[key]value is unordered
+	counts := make(map[string]int)
+
+	input := bufio.NewScanner(os.Stdin)
+
+	// Scan() advances scanner to new token
+	for input.Scan() {
+
+		// Gets string, Creates key in counts, Increases index for similar string
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d: %s\n", n, line)
+		}
+	}
+}
