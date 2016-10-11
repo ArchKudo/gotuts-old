30eef3efeda107ab2f193aa568e6c0c7ea42c895
ArchKudo <vgd.vyas17@gmail.com>
2016-10-11 23:57:59 +0530

    Added fetchall.go

---
+++ ch1/fetch/fetchall.go
@@ -0,0 +1,51 @@
+// fetchall.go Fetch websites concurrently
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	// Makes a new channel 
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+
+		// Starts a new goroutine (thread)
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+
+		// Recieve value from ch
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("While reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s\n", secs, nbytes, url)
+}
