86b1b6cd31dee8de3dab03630c64baab77853ee7
ArchKudo <vgd.vyas17@gmail.com>
2016-10-12 00:44:29 +0530

    Added server

---
+++ ch1/server/server.go
@@ -0,0 +1,18 @@
+// server.go A minimal echo server.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8321", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
