c0a6fa1560f561c5a48cfe9ac1ba72ae8bb2723a
ArchKudo <vgd.vyas17@gmail.com>
2016-10-14 23:15:28 +0530

    Added echo.go

---
+++ ch2/echo.go
@@ -0,0 +1,27 @@
+// echo.go Print cmdline args with flags
+
+package echo
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+// Create flags for use in cmdline args
+
+// Flag of type Bool
+var n = flag.Bool("n", false, "omit trailing spaces")
+
+// Flag of type String
+var sep = flag.String("s", " ", "seperator")
+
+func main() {
+	flag.Parse()
+	fmt.Println(strings.Join(flag.Args(), *sep))
+
+	// sep, n are pointers to Flag
+	if !*n {
+		fmt.Println()
+	}
+}
